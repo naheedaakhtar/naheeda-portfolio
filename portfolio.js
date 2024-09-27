4ec3f0604dddb7b55d2b2a648b3c9688472a363d
Naheeda <nakhtar@colgate.edu>
2024-09-27 15:01:09 -0400

    New base structure

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+// You can add JavaScript here for additional interactivity
+// For example, smooth scrolling to sections or a mobile menu toggle
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
